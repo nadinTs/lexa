eae3eefa135938d18700eb6d997ad84217008f79
nadinTs <nadin@gmail.com>
2015-02-06 22:56:17 +0300

    правка ошибок в инфе увеличился межстрочный интервал

--- js/main.js
+++ js/main.js
@@ -5,13 +5,6 @@ $('#slider').cycle({
 
 });
 
-$(function(){
-    $('.spoiler').hide();
-    $('<span class="revealer">Подробнее...</span>').insertBefore('.spoiler');
-    $('.revealer').click(function(){
-        $(this).hide();
-        $(this).next().fadeIn();
-    });
-});
+
 
 
